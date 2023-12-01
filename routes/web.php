91a691262bdd58336b403064a88d6d889fb25762
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-12-01 00:25:21 -0300

    refactoring routes file add auth middleware

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,12 @@ Route::get('/dashboard', DashboardController::class)
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::post('/question/store', [QuestionController::class, 'store'])->name('question.store');
-
-Route::post('/question/like/{question}', LikeController::class)->name('question.like');
-Route::post('/question/unlike/{question}', UnlikeController::class)->name('question.unlike');
-Route::put('/question/publish/{question}', PublishController::class)->name('question.publish');
+Route::middleware('auth')->group(function () {
+    Route::post('/question/store', [QuestionController::class, 'store'])->name('question.store');
+    Route::post('/question/like/{question}', LikeController::class)->name('question.like');
+    Route::post('/question/unlike/{question}', UnlikeController::class)->name('question.unlike');
+    Route::put('/question/publish/{question}', PublishController::class)->name('question.publish');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
--- tests/Feature/Question/CreateTest.php
+++ tests/Feature/Question/CreateTest.php
@@ -19,7 +19,6 @@ test('should be able to create a new question bigger than 255 characters', funct
     assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?']);
 });
 
-
 test('should check if ends with question mark ?', function () {
     $user = User::factory()->create();
     actingAs($user);
@@ -61,4 +60,10 @@ test('should create as a draft all the time', function () {
         'draft'    => true
     ]);
 
+});
+
+test('only authenticated users can create a new question', function () {
+    post(route('question.store'), [
+        'question' => str_repeat('*', 8) . '?',
+    ])->assertRedirect(route('login'));
 });
